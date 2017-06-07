8e2cf434d5c9750d003978642b42004954f72cd8
Meagan Blais <meaganblais@gmail.com>
2017-06-06 20:36:28 -0400

    adding url shortening pt.1

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080; 
+const bodyParser = require("body-parser");
 
 app.set('view engine', 'ejs');
 
@@ -8,6 +9,8 @@ const urlDatabase = {
     'b2xVn2': 'http://www.lighthouselabs.ca',
     '9sm5xK': 'http://www.google.com'
 };
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.end('Hello!');
@@ -26,11 +29,31 @@ app.get('/urls', (req, res) => {
     res.render('urls_index', templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get('/urls/:id', (req, res) => {
     let templateVars = { shortURL: req.params.id, url: urlDatabase[req.params.id]};
     res.render('urls_show', templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString() {
+  let result = '';
+  let charset = 'abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  for (var i=0; i < 6; i++) {
+    result += charset[Math.floor(Math.random() * charset.length)];
+  }
+  return result;
+}
+generateRandomString()
+
